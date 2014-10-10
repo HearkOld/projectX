7b408e6509ae8a0e99caacbfe819506d07dcfc17
Heark <hearkofsorcerey@gmail.com>
2014-10-10 11:08:49 -0400

    Update rockps.js

--- games/rockps.js
+++ games/rockps.js
@@ -2,3 +2,5 @@
 //1st Game by Heark || //
 /////////////////////////
 var rps;
+var computerItems = ["Rock", "Paper", "Scissors"]
+var computerChoice = computerItems[Math.floor(Math.random()*computerItems.length)]

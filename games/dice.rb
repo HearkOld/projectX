0f66729ff815ac2a5a5d2ebd5ee273ad806bfa40
Heark <hearkofsorcerey@gmail.com>
2014-10-20 14:59:36 -0400

    Update dice.rb

--- games/dice.rb
+++ games/dice.rb
@@ -1,30 +1,43 @@
+//The code starts here
 
-
-setFont( 'Chiller', 35, 'bold')
+setFont( 'Papyrus', 16.5, 'bold')
 
 // Variable Storage
 
 $c = getControls()
-$die = rand(6)
-$die2 = rand(6)
+$die = [1, 2, 3, 4, 5, 6]
+$scores = []
+$die2 = [1, 2, 3, 4, 5, 6]
+$result = $die.Random()
 $dice = new Image('dice.png')
+$start = false
 // Regular Screen
+fill( :black )
+def Roll()
+    $result = $die.Random()
+    $start = true
+    $result2 = $die2.Random()
+    $score = $result + $result2
+    $scores.add($score)
+end
 onEachFrame() do
-    fill( :black )
+    fill(:black)
+    if $start == true
+        fillText("You rolled a " + $result + " and a " + $result2 + " for a score of " + $score + " your previous scores are: " +$scores+ ".", 10, 25)
+    end
     setColor( :white )
-    fillText("Roll the Dice!", 10, 35)
+    if getControls().isKeyPressed( :x )
+        Roll()
+    end
+    fillText("Roll the Dice!", 15, 55)
     drawImage($dice, 50, 50)
     drawImage($dice, 425, 225, 10)
+    fillText("Press X To roll!", 199, 355)
 end
+
     class user
         def setScore(score)
             @score = score
         end
     end
 c = getControls()
-mouseX = c.getMouseX()
-mouseY = c.getMouseY()
-
-if getControls().isKeyDown( :x )
-    console( " You rolled a " +$die+ "!")
-end
